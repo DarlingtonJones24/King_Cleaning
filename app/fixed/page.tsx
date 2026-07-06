f9da8e4c14e5237a28073c8966591cdb5876d48c
DJone21111111 <144218093+DJone21111111@users.noreply.github.com>
2026-07-06 22:37:36 +0200

    Fix duplicate gallery cards using local images instead of broken fallback

--- app/fixed/page.tsx
+++ app/fixed/page.tsx
@@ -13,20 +13,14 @@ const images = {
   founder: "/king-prosper.jpg",
   planning: "/Leader.PNG",
   team: "/Cleaners1.PNG",
-  quality:
-    "https://cdn.pixabay.com/photo/2017/07/29/22/20/clipboard-863418_1280.jpg",
-  offices:
-    "https://cdn.pixabay.com/photo/2020/09/04/06/19/cleaning-5546204_1280.jpg",
-  schools:
-    "https://cdn.pixabay.com/photo/2017/07/06/23/04/classroom-2477803_1280.jpg",
-  gyms:
-    "https://cdn.pixabay.com/photo/2020/04/06/13/37/exercise-5006768_1280.jpg",
+  quality: "/king-slide-1.png",
+  offices: "/Cleaners1.PNG",
+  schools: "/Leader.PNG",
+  gyms: "/king-slide-1.png",
   staircases:
     "https://cdn.pixabay.com/photo/2017/08/07/14/15/staircase-2596825_1280.jpg",
-  restaurants:
-    "https://cdn.pixabay.com/photo/2017/08/11/07/53/restaurant-2629785_1280.jpg",
-  hotels:
-    "https://cdn.pixabay.com/photo/2016/08/05/08/27/hotel-1579355_1280.jpg"
+  restaurants: "/king-prosper.jpg",
+  hotels: "/king-slide-2.png"
 };
 
 const fallbackImage = "/Cleaners1.PNG";
@@ -667,7 +661,6 @@ export default function FixedHomePage() {
                   src={item.image}
                   alt={"alt" in item && item.alt ? item.alt : `${item.label} cleaning service`}
                   loading="lazy"
-                  onError={handleImageError}
                   style={{ objectPosition: galleryFocus[index] ?? "center" }}
                 />
                 <span>{item.label}</span>
